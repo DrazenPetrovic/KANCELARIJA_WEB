5114d95dd139d6e83d667c2406000b4de95dba66
Drazen-Posao <drazenpetrovic01@gmail.com>
2026-03-23 09:29:03 +0100

    izmjena ENV

--- src/config/env.js
+++ src/config/env.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const env = {
-  PORT: process.env.PORT || 3002,
+  PORT: process.env.PORT || 3001,
   NODE_ENV: process.env.NODE_ENV || "development",
   FRONTEND_URL: process.env.FRONTEND_URL || "http://localhost:5173",
   JWT_SECRET: process.env.JWT_SECRET || "change-me",
